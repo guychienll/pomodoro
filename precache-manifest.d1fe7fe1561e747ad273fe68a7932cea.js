86248c19f92b436b45b02e934f3361dd7c3126e1
guychien <guychienll@gmail.com>
2020-04-22 00:54:46 +0800

    Updates

---
+++ precache-manifest.d1fe7fe1561e747ad273fe68a7932cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e31bf30cdfb05cfa70ea0502887624a",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "1a3ec11a8c40e6f83c27",
+    "url": "/pomodoro/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "92ba4dd2ad68a5ac39ee",
+    "url": "/pomodoro/static/js/2.0304483e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.0304483e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3ec11a8c40e6f83c27",
+    "url": "/pomodoro/static/js/main.d3df3a62.chunk.js"
+  },
+  {
+    "revision": "3c8bf5feb8d4a3bfbdaf",
+    "url": "/pomodoro/static/js/runtime-main.5f33536a.js"
+  }
+]);
