4075d5f7c787bf094923b1bb78fc46f144df5186
guychien <guychienll@gmail.com>
2020-04-22 09:49:26 +0800

    Updates

---
+++ precache-manifest.1bcc049577c5742608e1d95a22ab6aab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9d40fe9658f2d2bc9339ce438da7ac",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "9ee1feeb0745632bcdc6",
+    "url": "/pomodoro/static/css/main.648d8f63.chunk.css"
+  },
+  {
+    "revision": "c80a3c352a73ab8d34dc",
+    "url": "/pomodoro/static/js/2.8d10764b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pomodoro/static/js/2.8d10764b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee1feeb0745632bcdc6",
+    "url": "/pomodoro/static/js/main.0836ffce.chunk.js"
+  },
+  {
+    "revision": "3c8bf5feb8d4a3bfbdaf",
+    "url": "/pomodoro/static/js/runtime-main.5f33536a.js"
+  },
+  {
+    "revision": "30013910784e4f7bf6e1714268c7d06f",
+    "url": "/pomodoro/static/media/add_red.30013910.svg"
+  },
+  {
+    "revision": "32e0817ace0055170630751b47450aa7",
+    "url": "/pomodoro/static/media/add_white.32e0817a.svg"
+  },
+  {
+    "revision": "1998e7ea78461318f35ac6ecb9406018",
+    "url": "/pomodoro/static/media/analysis_red.1998e7ea.svg"
+  },
+  {
+    "revision": "522ba8bee192d5df0a3eaedb5189b2a1",
+    "url": "/pomodoro/static/media/analysis_white.522ba8be.svg"
+  },
+  {
+    "revision": "f6f4664c5affd38c1aa5765db4ef38a7",
+    "url": "/pomodoro/static/media/list_red.f6f4664c.svg"
+  },
+  {
+    "revision": "99b596caa2efee46c92373ab4bea4bf1",
+    "url": "/pomodoro/static/media/list_white.99b596ca.svg"
+  },
+  {
+    "revision": "1e25b3f452a8c966a8537cde5acd265e",
+    "url": "/pomodoro/static/media/ringtone_red.1e25b3f4.svg"
+  },
+  {
+    "revision": "c1791935b805ce1c49248f6d822a98b1",
+    "url": "/pomodoro/static/media/ringtone_white.c1791935.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro/precache-manifest.d1fe7fe1561e747ad273fe68a7932cea.js"
+  "/pomodoro/precache-manifest.1bcc049577c5742608e1d95a22ab6aab.js"
 );
 
 self.addEventListener('message', (event) => {
